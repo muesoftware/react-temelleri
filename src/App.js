c638e588bdc5e5382e9c6c0a814070ff187bf667
muesoftware <muedeveloper@gmail.com>
2024-06-01 07:21:37 +0300

    ilk react kodları

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      ReactJS
+      <img src={logo} height={40} width={40}/>
+    </div>
+  );
+}
+
+export default App;
